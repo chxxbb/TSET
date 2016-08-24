c78740f8dd09460c0b8874bbc9b6e0eab6d0d546
chxxbb <chxxbb@outlook.com>
2016-08-24 11:29:00 +0800

    2016年8月24日 11:28:43
           新增:
           1.注册页面
           2.忘记密码页面

---
+++ app/src/main/java/com/example/chen/tset/FindpawActivity.java
@@ -0,0 +1,18 @@
+package com.example.chen.tset;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.chen.tset.R;
+
+public class FindpawActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_pawnumber);
+    }
+}
---
+++ app/src/main/java/com/example/chen/tset/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.example.chen.tset;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.chen.tset.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
