9da516039820cc350bc1caaf4f4babc00d3f32c6
李禹江 <583310782@qq.com>
2016-08-27 11:37:27 +0800

    2016年8月27日11:36:01
    新增：
    主界面修改
    主题选择跳转

--- app/src/main/java/com/example/chen/tset/View/SetPageActivity.java
+++ app/src/main/java/com/example/chen/tset/View/SetPageActivity.java
@@ -1,15 +1,35 @@
 package com.example.chen.tset.View;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.example.chen.tset.R;
 
-public class SetPageActivity extends AppCompatActivity {
+public class SetPageActivity extends AppCompatActivity implements View.OnClickListener {
+    private RelativeLayout rl_themese;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_page);
+        findView();
+    }
+
+    private void findView() {
+        rl_themese = (RelativeLayout) findViewById(R.id.rl_themese);
+        rl_themese.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.rl_themese:
+                Intent intent = new Intent(SetPageActivity.this, ThemeselectActivity.class);
+                startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/chen/tset/page/LectureroomAdapter.java
+++ app/src/main/java/com/example/chen/tset/page/LectureroomAdapter.java
@@ -15,46 +15,83 @@ import java.util.List;
 /**
  * Created by Administrator on 2016/8/26 0026.
  */
-public class LectureroomAdapter extends BaseAdapter {
+public class LectureroomAdapter extends RecyclerView.Adapter {
     List<String> list;
     Context context;
+    LayoutInflater inflater;
 
     public LectureroomAdapter(Context context, List<String> list) {
         this.context = context;
         this.list = list;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    class Viewholder extends RecyclerView.ViewHolder {
+        private TextView tv_lectr;
+
+        public Viewholder(View itemView) {
+            super(itemView);
+            tv_lectr = (TextView) itemView.findViewById(R.id.tv_lectr);
+        }
     }
 
     @Override
-    public int getCount() {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.lectureroom_item, parent, false);
+        return new Viewholder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        Viewholder viewholder = (Viewholder) holder;
+        viewholder.tv_lectr.setText(list.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
         return list.size();
     }
 
-    @Override
-    public String getItem(int position) {
-        return list.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-            convertView = inflater.inflate(R.layout.lectureroom_item, parent, false);
-            convertView.setTag(new ViewHolder(convertView));
-        }
-        ViewHolder viewHolder = (ViewHolder) convertView.getTag();
-        viewHolder.tv_lectr.setText(list.get(position));
-        return convertView;
-    }
-
-    static class ViewHolder {
-        private TextView tv_lectr;
-
-        ViewHolder(View v) {
-            tv_lectr = (TextView) v.findViewById(R.id.tv_lectr);
-        }
-    }
+//    List<String> list;
+//    Context context;
+//
+//    public LectureroomAdapter(Context context, List<String> list) {
+//        this.context = context;
+//        this.list = list;
+//    }
+//
+//    @Override
+//    public int getCount() {
+//        return list.size();
+//    }
+//
+//    @Override
+//    public String getItem(int position) {
+//        return list.get(position);
+//    }
+//
+//    @Override
+//    public long getItemId(int position) {
+//        return position;
+//    }
+//
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//        if (convertView == null) {
+//            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+//            convertView = inflater.inflate(R.layout.lectureroom_item, parent, false);
+//            convertView.setTag(new ViewHolder(convertView));
+//        }
+//        ViewHolder viewHolder = (ViewHolder) convertView.getTag();
+//        viewHolder.tv_lectr.setText(list.get(position));
+//        return convertView;
+//    }
+//
+//    static class ViewHolder {
+//        private TextView tv_lectr;
+//
+//        ViewHolder(View v) {
+//            tv_lectr = (TextView) v.findViewById(R.id.tv_lectr);
+//        }
+//    }
 }
