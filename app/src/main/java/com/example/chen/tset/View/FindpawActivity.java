44d1c2d3476358bf628888f3fcf69fb929960746
chxxbb <chxxbb@outlook.com>
2016-08-27 10:49:46 +0800

    2016年8月27日 10:49:09
    修改:
    1.登录界面的手机号窗口只能输入数字了

--- app/src/main/java/com/example/chen/tset/View/FindpawActivity.java
+++ app/src/main/java/com/example/chen/tset/View/FindpawActivity.java
@@ -14,6 +14,6 @@ public class FindpawActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_pawnumber);
-        //4334
+
     }
 }
