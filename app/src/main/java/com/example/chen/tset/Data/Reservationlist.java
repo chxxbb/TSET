6fa8cffad3d5a5bbd4b7d1822c015cdd503ef302
李禹江 <583310782@qq.com>
2016-09-07 10:04:33 +0800

    2016年9月7日10:04:16
    新增：
    预约详情与后台数据交互

---
+++ app/src/main/java/com/example/chen/tset/Data/Reservationlist.java
@@ -0,0 +1,140 @@
+package com.example.chen.tset.Data;
+
+/**
+ * Created by Administrator on 2016/9/7 0007.
+ */
+public class Reservationlist {
+    private String doctor_name;
+    private String icon;
+    private String patient_name;
+    private String section;
+    private String order_no;
+    private String content;
+    private String title;
+    private String appointment_time;
+    private String valid_time;
+    private String address;
+    private String patient_phone;
+    private String money;
+    private String hospital;
+
+    public Reservationlist(String doctor_name, String icon, String patient_name, String section, String order_no, String content, String title, String appointment_time, String valid_time, String address, String patient_phone, String money, String hospital) {
+        this.doctor_name = doctor_name;
+        this.icon = icon;
+        this.patient_name = patient_name;
+        this.section = section;
+        this.order_no = order_no;
+        this.content = content;
+        this.title = title;
+        this.appointment_time = appointment_time;
+        this.valid_time = valid_time;
+        this.address = address;
+        this.patient_phone = patient_phone;
+        this.money = money;
+        this.hospital = hospital;
+    }
+
+    public String getDoctor_name() {
+        return doctor_name;
+    }
+
+    public void setDoctor_name(String doctor_name) {
+        this.doctor_name = doctor_name;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getPatient_name() {
+        return patient_name;
+    }
+
+    public void setPatient_name(String patient_name) {
+        this.patient_name = patient_name;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public String getOrder_no() {
+        return order_no;
+    }
+
+    public void setOrder_no(String order_no) {
+        this.order_no = order_no;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAppointment_time() {
+        return appointment_time;
+    }
+
+    public void setAppointment_time(String appointment_time) {
+        this.appointment_time = appointment_time;
+    }
+
+    public String getValid_time() {
+        return valid_time;
+    }
+
+    public void setValid_time(String valid_time) {
+        this.valid_time = valid_time;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPatient_phone() {
+        return patient_phone;
+    }
+
+    public void setPatient_phone(String patient_phone) {
+        this.patient_phone = patient_phone;
+    }
+
+    public String getMoney() {
+        return money;
+    }
+
+    public void setMoney(String money) {
+        this.money = money;
+    }
+
+    public String getHospital() {
+        return hospital;
+    }
+
+    public void setHospital(String hospital) {
+        this.hospital = hospital;
+    }
+}
--- app/src/main/java/com/example/chen/tset/View/ReservationlistActivity.java
+++ app/src/main/java/com/example/chen/tset/View/ReservationlistActivity.java
@@ -2,15 +2,38 @@ package com.example.chen.tset.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.chen.tset.Data.Http_data;
+import com.example.chen.tset.Data.Reservation;
+import com.example.chen.tset.Data.Reservationlist;
+import com.example.chen.tset.Data.User;
 import com.example.chen.tset.R;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import okhttp3.Call;
 
 public class ReservationlistActivity extends AppCompatActivity implements View.OnClickListener {
     private LinearLayout ll_myreservationg;
     private ScrollView scrollView;
+    private TextView tv_content, tv_doctor_name, tv_title, tv_appointment_time, tv_valid_time, tv_address, tv_patient_name, tv_money, tv_patient_phone, tv_order_no, tv_hospital, tv_section, tv_doctor_section;
+    private CircleImageView iv_icon;
+    Gson gson = new Gson();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +42,65 @@ public class ReservationlistActivity extends AppCompatActivity implements View.O
         scrollView = (ScrollView) findViewById(R.id.scrollView);
         scrollView.setVerticalScrollBarEnabled(false);
         findView();
+        httpinit();
     }
+
 
     private void findView() {
         ll_myreservationg = (LinearLayout) findViewById(R.id.ll_myreservationg);
+        tv_content = (TextView) findViewById(R.id.tv_content);
+        tv_doctor_name = (TextView) findViewById(R.id.tv_doctor_name);
+        tv_title = (TextView) findViewById(R.id.tv_title);
+        tv_appointment_time = (TextView) findViewById(R.id.tv_appointment_time);
+        tv_valid_time = (TextView) findViewById(R.id.tv_valid_time);
+        tv_address = (TextView) findViewById(R.id.tv_address);
+        tv_patient_name = (TextView) findViewById(R.id.tv_patient_name);
+        tv_money = (TextView) findViewById(R.id.tv_money);
+        tv_patient_phone = (TextView) findViewById(R.id.tv_patient_phone);
+        tv_order_no = (TextView) findViewById(R.id.tv_order_no);
+        tv_hospital = (TextView) findViewById(R.id.tv_hospital);
+        tv_section = (TextView) findViewById(R.id.tv_section);
+        tv_doctor_section = (TextView) findViewById(R.id.tv_doctor_section);
+        iv_icon = (CircleImageView) findViewById(R.id.iv_icon);
         ll_myreservationg.setOnClickListener(this);
 
+    }
+
+    private void httpinit() {
+        OkHttpUtils
+                .post()
+                .url(Http_data.http_data + "/findBookingDetails")
+                .addParams("id", "1")
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        Toast.makeText(ReservationlistActivity.this, "网络连接失败", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        Log.e("预约详情返回", response);
+                        Type listtype = new TypeToken<LinkedList<Reservationlist>>() {
+                        }.getType();
+                        LinkedList<Reservationlist> leclist = gson.fromJson(response, listtype);
+                        Iterator it = leclist.iterator();
+                        Reservationlist reservationlist = (Reservationlist) it.next();
+                        tv_content.setText(reservationlist.getContent());
+                        tv_doctor_name.setText(reservationlist.getDoctor_name());
+                        tv_title.setText("职称：" + reservationlist.getTitle());
+                        tv_appointment_time.setText("日期：" + reservationlist.getAppointment_time());
+                        tv_valid_time.setText("有效期：" + reservationlist.getValid_time());
+                        tv_address.setText("地址：" + reservationlist.getAddress());
+                        tv_patient_name.setText("姓名：" + reservationlist.getPatient_name());
+                        tv_money.setText("价格：￥" + reservationlist.getMoney());
+                        tv_patient_phone.setText("电话号码：" + reservationlist.getPatient_phone());
+                        tv_order_no.setText("订单号：" + reservationlist.getOrder_no());
+                        tv_hospital.setText(reservationlist.getHospital());
+                        tv_section.setText("科室：" + reservationlist.getSection());
+                        tv_doctor_section.setText(reservationlist.getSection());
+                    }
+                });
     }
 
     @Override
