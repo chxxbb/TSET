3ada46101cea1d6d84e92aac7cf0830b81a86336
李禹江 <583310782@qq.com>
2016-09-07 11:53:58 +0800

    2016年9月7日11:53:52
    新增：
    主题选择页面修改

--- app/src/main/java/com/example/chen/tset/View/ThemeselectActivity.java
+++ app/src/main/java/com/example/chen/tset/View/ThemeselectActivity.java
@@ -4,11 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
 
 import com.example.chen.tset.R;
 
 public class ThemeselectActivity extends AppCompatActivity implements View.OnClickListener {
     private LinearLayout ll_set;
+    private RadioButton rbtn_white_front, rbtn_white_queen, rbtn_blue_front, rbtn_blue_queen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +21,22 @@ public class ThemeselectActivity extends AppCompatActivity implements View.OnCli
 
     private void finView() {
         ll_set = (LinearLayout) findViewById(R.id.ll_set);
+        rbtn_white_front = (RadioButton) findViewById(R.id.rbtn_white_front);
+        rbtn_white_queen = (RadioButton) findViewById(R.id.rbtn_white_queen);
+        rbtn_blue_front = (RadioButton) findViewById(R.id.rbtn_blue_front);
+        rbtn_blue_queen = (RadioButton) findViewById(R.id.rbtn_blue_queen);
+        rbtn_white_queen.setChecked(true);
+        rbtn_white_queen.setText("使用中");
+        rbtn_white_front.setChecked(true);
+        rbtn_blue_front.setChecked(false);
+        rbtn_blue_queen.setChecked(false);
+        rbtn_blue_queen.setText("使用");
+        rbtn_blue_front.setEnabled(false);
+        rbtn_white_front.setEnabled(false);
         ll_set.setOnClickListener(this);
+        rbtn_white_queen.setOnClickListener(this);
+        rbtn_blue_queen.setOnClickListener(this);
+
     }
 
     @Override
@@ -28,6 +45,19 @@ public class ThemeselectActivity extends AppCompatActivity implements View.OnCli
             case R.id.ll_set:
                 finish();
                 break;
+            case R.id.rbtn_white_queen:
+                rbtn_white_queen.setText("使用中");
+                rbtn_white_front.setChecked(true);
+                rbtn_blue_front.setChecked(false);
+                rbtn_blue_queen.setChecked(false);
+                rbtn_blue_queen.setText("使用");
+                break;
+            case R.id.rbtn_blue_queen:
+                rbtn_blue_queen.setText("使用中");
+                rbtn_white_queen.setText("使用");
+                rbtn_white_front.setChecked(false);
+                rbtn_white_queen.setChecked(false);
+                rbtn_blue_front.setChecked(true);
         }
     }
 }
